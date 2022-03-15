a397bc375b41b0b434b01b3a5e24475f7e1b19ed
RakhiPatil <rakhipatil2608@gmail.com>
2022-03-15 00:25:39 -0700

    MODIFIED: Using Equals Method

--- src/com/infogalaxy/LineComparisonProblem.java
+++ src/com/infogalaxy/LineComparisonProblem.java
@@ -3,24 +3,40 @@ package com.infogalaxy;
 import java.util.Scanner;
 
 public class LineComparisonProblem {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to line Comparison problem");
-        Scanner scanner = new Scanner(System.in);
-        int x1;
-        int x2;
-        int y1;
-        int y2;
-        double length;
-        System.out.println("Enter the Co-ordinates for starting point:");
-        x1 = scanner.nextInt();
-        y1 = scanner.nextInt();
-        System.out.println("Enter the Co-ordinates for ending point: ");
-        x2 = scanner.nextInt();
-        y2 = scanner.nextInt();
-        System.out.println("x1 : "+x1+"\ny1 : "+y1+"\nx2 : "+x2+"\ny2 : "+y2);
-        length = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
-        System.out.println("Lenth of The Line : "+length);
+    public static String equals(double length, double length2){
+        if(length == length2){
+           return "Lines are of Equal length";
+        }else{
+            return  "Lines are Unequal in Length";
+        }
     }
 
+    public static void main(String[] args) {
+        //UC-1
+        System.out.println("Welcome to line Comparison problem");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Co-ordinates for starting point:");
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        System.out.println("Enter the Co-ordinates for ending point: ");
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+        System.out.println("x1 : "+x1+"\ny1 : "+y1+"\nx2 : "+x2+"\ny2 : "+y2);
+        double length = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+        System.out.println("Length of The First Line : "+length);
+
+        //UC-2
+        System.out.println("Enter the Co-ordinates for starting point:");
+        int a1 = scanner.nextInt();
+        int b1 = scanner.nextInt();
+        System.out.println("Enter the Co-ordinates for ending point: ");
+        int a2 = scanner.nextInt();
+        int b2 = scanner.nextInt();
+        System.out.println("a1 : "+a1+"\nb1 : "+b1+"\na2 : "+a2+"\nb2 : "+b2);
+        double length2 = Math.sqrt(Math.pow((a2-a1),2) + Math.pow((b2-b1),2));
+        System.out.println("Length of The Second Line : "+length2);
+
+        System.out.println(equals(length,length2));
+
+    }
 }
